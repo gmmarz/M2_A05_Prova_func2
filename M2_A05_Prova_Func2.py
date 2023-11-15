457fc6451f580e397f3dbd322be3066f91a3c988
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-11-14 22:21:41 -0300

    Resolução e euniciado da prova 2

--- M2_A05_Prova_Func2.py
+++ M2_A05_Prova_Func2.py
@@ -11,3 +11,34 @@
 # 3.Função reduce()  para obter a soma de todos os números da lista numeros
 
 # -Qual o resultado obtido após a execução das operações acima?
+
+#Importando a biblioteca para poder utilizar a função reduce do exercicío 3
+from functools import reduce
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f'\nEsta é a lista dada para a prova:  {numeros}')
+print('-'*30)
+
+print('Função map() para obter uma nova lista com o quadrado de cada número da lista numeros')
+
+quadrados_lst = list(map(lambda x: x**2,numeros ))
+
+print(f'Resposta da função map, fazendo o quadrado de cada membro de numeros, transformada em lista é {quadrados_lst}')
+
+print('-'*30)
+print('2.Função filter() para obter uma nova lista com números pares da lista numeros')
+
+pares = list(filter(lambda x: x%2 ==0, numeros ))
+
+print(f'Resposta da função filter, filtrando apenas os numeros pares da lista numeros, transformada em lista é {pares}')
+
+print('-'*30)
+print('Função reduce()  para obter a soma de todos os números da lista numeros')
+
+soma = reduce(lambda x,y: x + y , numeros)
+
+print(f'Resposta da função reduce, somando os elementos da lista numeros, é {soma}')
+
+
