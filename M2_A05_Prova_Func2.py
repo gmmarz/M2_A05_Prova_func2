b2383ff8182ca7140ecbf0ba2ebccbcefc8dafad
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-11-14 22:03:26 -0300

    Prova enunciado

---
+++ M2_A05_Prova_Func2.py
@@ -0,0 +1,13 @@
+#Aluno: Guilherme
+#Enunciado -
+# [PY-A05] Considere uma lista de números inteiros 
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Utilizando as funções map(),filter() e reduce(), escreva um código que execute as seguintes operações:
+
+# 1.Função map() para obter uma nova lista com o quadrado de cada número da lista numeros
+
+# 2.Função filter() para obter uma nova lista com números pares da lista numeros
+
+# 3.Função reduce()  para obter a soma de todos os números da lista numeros
+
+# -Qual o resultado obtido após a execução das operações acima?
